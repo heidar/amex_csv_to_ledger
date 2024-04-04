34124fdb92d5ca7a18579e120a73eb3845441c09
Heidar Bernhardsson <heidar@heidarb.com>
2024-04-04 17:55:16 +0100

    add class to hold statement line

--- lib/amex_csv_to_ledger.rb
+++ lib/amex_csv_to_ledger.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'amex_csv_to_ledger/version'
+require_relative 'amex_csv_to_ledger/statement_line'
 
 module AmexCsvToLedger
   class Error < StandardError; end
---
+++ lib/amex_csv_to_ledger/statement_line.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'date'
+
+# Class to hold the data found in a line of an AMEX statement.
+class StatementLine
+  attr_reader :amount, :description
+
+  AMEX_DATE_FORMAT = '%Y/%m/%d'
+
+  def initialize(amount:, date:, description:)
+    @amount = amount
+    @date   = date
+    @description = description
+  end
+
+  def date
+    parsed_date.strftime('%Y-%m-%d')
+  end
+
+  private
+
+  def parsed_date
+    Date.parse(@date, AMEX_DATE_FORMAT)
+  end
+end
